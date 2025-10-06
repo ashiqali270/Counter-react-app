7a7d765651441a6fedb94ad6e3971796d5015611
Your Name <you@example.com>
2025-10-06 06:57:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  const [count, setCount] = useState(0);
+
+  const increment= () =>setCount(count + 1);
+  const decrement =() =>setCount(count - 1);
+  const reset= () => setCount(0);
+
+return (
+  <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
+      <h1 className="text-4xl font-bold mb-6"> Counter App</h1>
+
+ <div className="text-6xl font-extrabold mb-6">{count}</div>
+
+    <div className="space-x-4">
+        <button
+          onClick={increment}
+          className="px-6 py-2 bg-green-500 hover:bg-green-600 rounded-lg text-lg shadow-lg"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          Increment
+        </button>
+
+    <button
+          onClick={decrement}
+          className="px-6 py-2 bg-red-500 hover:bg-red-600 rounded-lg text-lg shadow-lg"
+        >
+          Decrement
+        </button>
+
+      <button
+          onClick={reset}
+          className="px-6 py-2 bg-yellow-500 hover:bg-yellow-600 rounded-lg text-lg shadow-lg"
+        >
+          Reset
+      </button>
+      </div>
+     </div>
+  );}
 
 export default App;
